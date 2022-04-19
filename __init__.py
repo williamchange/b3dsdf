7dd0bdabca1fd6acaebeda317653dacebe046e23
LMW <williamchange@gmail.com>
2022-04-19 22:08:38 +1000

    Bump version to `0.2.0`

--- __init__.py
+++ __init__.py
@@ -16,7 +16,7 @@ bl_info = {
     "author": "cmzw",
     "description": "A collection of 2D signed distance functions and utilities",
     "blender": (2, 83, 0),
-    "version": (0, 1, 1),
+    "version": (0, 2, 0),
     "location": "Shader Editor > Add > SDF",
     "tracker_url": "https://github.com/williamchange/b3dsdf/issues/new",
     "doc_url": "https://github.com/williamchange/b3dsdf/wiki/Examples",
